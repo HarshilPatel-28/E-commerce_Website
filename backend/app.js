28dde84797de2dd5b526d23381edbd7f785d463d
Harshil Patel <hp950036@gmail.com>
2023-06-25 16:06:16 +0530

    backend login and signUp feature added

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,10 @@ const errorMiddleware = require("./middleware/error")
 app.use(express.json())
 // Route Imports
 const product = require('./routes/productRoute');
+const user = require('./routes/userRoute')
 
 app.use("/api/v1",product); 
+app.use("/api/v1",user); 
 
 // Middleware for Error 
 app.use(errorMiddleware);
---
+++ backend/controllers/userController.js
@@ -0,0 +1,45 @@
+const ErrorHandler = require("../utils/errorHandler")
+const catchAsyncErrors = require("../middleware/catchAsyncError")
+const User = require('../models/userModel');
+const sendToken = require("../utils/jwtToken");
+
+// Register a user 
+exports.registerUser = catchAsyncErrors( async(req,res,next)=>{
+    const {name,email,password}=req.body;
+    const user = await User.create({
+        name,email,password,
+        avatar:{
+            public_id:"this is a sample id",
+            url:"profilePicUrl"
+        }
+    });
+
+   sendToken(user,201,res);
+});
+
+// Login User 
+exports.loginUser = catchAsyncErrors( async (req,res,next)=>{
+    const {email, password}= req.body;
+
+    // checking if user has given password and email both 
+
+    if(!email || !password){
+        return next(new ErrorHandler("Please Enter Email & Password",400));
+    }
+
+    const user = await User.findOne({email}).select("+password"); 
+
+    if(!user){
+        return next(new ErrorHandler("Invalid email or password",401))
+    }
+
+    const isPasswordMatched = user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password",401))
+
+    }
+
+    sendToken(user,200,res);
+
+})
